c45ba191e095d14df1494db39112c93559e437a9
vera-migel <115414615+vera-migel@users.noreply.github.com>
2024-06-06 15:39:02 +0300

    Update proba1.java

--- src/main/java/proba1.java
+++ src/main/java/proba1.java
@@ -2,10 +2,11 @@ public class proba1 {
     public static void main(String[] args) {
         System.out.println("Hello, World сначала!");
         System.out.println("Hello, World!");
-		System.out.println("Hello, что-то другое!");
+	System.out.println("Hello, что-то другое!");
         System.out.println("Hello, World еще раз!");
         System.out.println("Hello, World и второй раз!");
         System.out.println("Hello, World и третий раз!");
+	System.out.println("Hello, World и четвертый раз раз!");
     }
 }
 
