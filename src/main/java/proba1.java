4432bc4a03d1e1dd13ecfa8b3a3f4175d049412f
Vera Shishina <buterflyy@yandex.ru>
2024-06-06 12:13:53 +0300

    первая правка в релизной ветке

---
+++ src/main/java/proba1.java
@@ -0,0 +1,7 @@
+public class proba1 {
+    public static void main(String[] args) {
+        System.out.println("Hello, World сначала!");
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World еще раз!");
+    }
+}
